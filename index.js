ddafc1b999bf187c38401cb8c78ab6e37fc8a972
donell <donell.wambua@student.moringaschool.com>
2023-06-18 21:46:14 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Write your solution in this file!
+Kisii: "Migan Mutisya,"
+Kakamega:"Nixxie Mwelu,"
+Machakos:"Sasha Sampeiyan,"
+Jericho:"Mark Odera,"
+Somalia:"Gatete Njoroge,"
+
+
+function updateEmployeeWithKeyAndValue(employee,key,value){
+    const newEmployee={...employee}
+    newEmployee[key]=value
+    return newEmployee
+}
+
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee,key,value){
+    employee[key]=value
+    return employee
+}
+function deleteFromEmployeeBykey(employee,key){
+let newEmployee=(employee,'name')
+delete newemployee[key]
+return newEmployee
+
+
+}
+function destructivelyDeleteFromEmployeeByKey(employe,key){
+    let employee=(employee,'name')
+    delete employee.Machakos
+    return employee
+}
